06c37b724ed0062c8ed953a21dd47050ab05acfd
Wamm K. D <jaft.r@outlook.com>
2018-12-01 20:24:05 -0600

    Just a test; we'll see if I even end up going with this.

--- app/src/main/java/fediverse/writefreely/ekrilib/MainActivity.java
+++ app/src/main/java/fediverse/writefreely/ekrilib/MainActivity.java
@@ -2,11 +2,27 @@ package fediverse.writefreely.ekrilib;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View.OnClickListener;
+import android.view.View;
+import android.widget.EditText;
+import ru.noties.markwon.Markwon;
 
 public class MainActivity extends AppCompatActivity {
+	private EditText et;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		et = (EditText) findViewById(R.id.plain_text_input);
+
+		findViewById(R.id.butt).setOnClickListener(new OnClickListener() {
+		                                           	@Override
+		                                           	public void onClick(View view) {
+		                                           		et.setText(Markwon.markdown(MainActivity.this,
+		                                           		                            et.getText().toString()));
+		                                           	}
+		                                           });
 	}
 }
