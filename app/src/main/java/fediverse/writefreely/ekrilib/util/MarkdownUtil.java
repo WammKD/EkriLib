2d0e5c4691748f931d65fe46172a043e8b3cd8ba
Wamm K. D <jaft.r@outlook.com>
2018-12-02 18:15:29 -0600

    I'll probably end up junking this but 'going to add it, for now.

--- app/src/main/java/fediverse/writefreely/ekrilib/util/MarkdownUtil.java
+++ app/src/main/java/fediverse/writefreely/ekrilib/util/MarkdownUtil.java
@@ -1,13 +1,28 @@
 package fediverse.writefreely.ekrilib.util;
 
 public class MarkdownUtil {
-	private final static String      BOLD         = "(?m)(^|[^*\\\\])\\*\\*(.+?)\\*\\*([^*\\\\]|$)";
-	private final static String      BOLD_REPLACE = "$1\\\\\\*\\\\\\*\\*\\*$2\\*\\*\\\\\\*\\\\\\*$3";
 	private final static String ITALICIZE         = "(?m)(^|[^*\\\\])\\*([^*]+?)\\*([^*\\\\]|$)";
 	private final static String ITALICIZE_REPLACE = "$1\\\\\\*\\*$2\\*\\\\\\*$3";
+	private final static String      BOLD         = "(?m)(^|[^*\\\\])\\*\\*([^*]*?[^*]+?)\\*\\*([^*\\\\]|$)";
+	private final static String      BOLD_REPLACE = "$1\\\\\\*\\\\\\*\\*\\*$2\\*\\*\\\\\\*\\\\\\*$3";
+	private final static String      BOTH         = "(?m)(^|[^*\\\\])\\*\\*\\*([^*]*?[^*]*?[^*]+?)\\*\\*\\*([^*\\\\]|$)";
+	private final static String      BOTH_REPLACE = "$1\\\\\\*\\\\\\*\\\\\\*\\*\\*\\*$2\\*\\*\\*\\\\\\*\\\\\\*\\\\\\*$3";
+	private final static String    STRIKE         = "(?m)(^|[^~\\\\])~~(.+?)~~([^~\\\\]|$)";
+	private final static String    STRIKE_REPLACE = "$1\\\\~\\\\~~~$2~~\\\\~\\\\~$3";
+	private final static String  HEADER_1         = "(?m)^# (.*)$";
+	private final static String  HEADER_1_REPLACE = "# \\\\\\# $1";
+	private final static String  HEADER_2         = "(?m)^## (.*)$";
+	private final static String  HEADER_2_REPLACE = "## \\\\\\#\\\\\\# $1";
+	private final static String  HEADER_3         = "(?m)^### (.*)$";
+	private final static String  HEADER_3_REPLACE = "### \\\\\\#\\\\\\#\\\\\\# $1";
+	private final static String  HEADER_4         = "(?m)^#### (.*)$";
+	private final static String  HEADER_4_REPLACE = "#### \\\\\\#\\\\\\#\\\\\\#\\\\\\# $1";
+	private final static String  HEADER_5         = "(?m)^##### (.*)$";
+	private final static String  HEADER_5_REPLACE = "##### \\\\\\#\\\\\\#\\\\\\#\\\\\\#\\\\\\# $1";
+	private final static String  HEADER_6         = "(?m)^###### (.*)$";
+	private final static String  HEADER_6_REPLACE = "###### \\\\\\#\\\\\\#\\\\\\#\\\\\\#\\\\\\#\\\\\\# $1";
 
 	public static String conserveMarkdown(final String content) {
-		return content.replaceAll(     BOLD,      BOLD_REPLACE)
-		              .replaceAll(ITALICIZE, ITALICIZE_REPLACE);
+		return content;
 	}
 }
