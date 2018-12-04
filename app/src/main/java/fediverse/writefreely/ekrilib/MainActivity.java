9476ff4af6b139bdafdde6a0a09a9d8396bd2538
Wamm K. D <jaft.r@outlook.com>
2018-12-03 22:04:33 -0600

    Cleanup, really.

--- app/src/main/java/fediverse/writefreely/ekrilib/MainActivity.java
+++ app/src/main/java/fediverse/writefreely/ekrilib/MainActivity.java
@@ -2,20 +2,16 @@ package fediverse.writefreely.ekrilib;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View.OnClickListener;
-import android.view.View;
-import android.widget.EditText;
 import com.yydcdut.markdown.MarkdownConfiguration.Builder;
 import com.yydcdut.markdown.MarkdownEditText;
 import com.yydcdut.markdown.MarkdownProcessor;
 import com.yydcdut.markdown.syntax.edit.EditFactory;
-import fediverse.writefreely.ekrilib.util.MarkdownUtil;
 
 public class MainActivity extends AppCompatActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		super.setContentView(R.layout.activity_main);
 
 		MarkdownProcessor markdownProcessor = new MarkdownProcessor(this);
 		markdownProcessor.config(new Builder(this).build());
