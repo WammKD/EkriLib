0fe300eb89e74e17aeb155d907bc7a78b877a49a
Wamm K. D <jaft.r@outlook.com>
2019-01-13 01:44:46 -0600

    Adjust this to pull collections in; while Drafts will probably be the only constant across all accounts, might as well keep the whole String array so I can easily add (and remove) Strings as needed. 'Wish Java wasn't so verbose for something so simple but oh well.

--- app/src/main/java/fediverse/writefreely/ekrilib/MainActivity.java
+++ app/src/main/java/fediverse/writefreely/ekrilib/MainActivity.java
@@ -1,14 +1,22 @@
 package fediverse.writefreely.ekrilib;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Pair;
 import android.view.Menu;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 import com.yydcdut.markdown.MarkdownConfiguration.Builder;
 import com.yydcdut.markdown.MarkdownEditText;
 import com.yydcdut.markdown.MarkdownProcessor;
 import com.yydcdut.markdown.syntax.edit.EditFactory;
+import fediverse.writefreely.api.model.Collection;
+import java.io.IOException;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 	@Override
@@ -26,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
 		super.findViewById(R.id.main_view_blogs)
 		     .setOnClickListener((final View v) -> startActivity(new Intent(MainActivity.this,
 		                                                                    BlogsActivity.class)));
+
+		new MyTask().execute((Spinner) super.findViewById(R.id.main_spinner_nav));
 	}
 
 	@Override
@@ -33,5 +43,48 @@ public class MainActivity extends AppCompatActivity {
 		super.getMenuInflater().inflate(R.menu.menu_main, menu);
 
 		return true;
+	}
+
+	private class MyTask extends AsyncTask<Spinner, Void, Pair<Spinner,
+	                                                           Collection[]>> {
+		@Override
+		protected Pair<Spinner, Collection[]> doInBackground(Spinner... params) {
+			try {
+				return Pair.create(params[0],
+				                   LoginActivity.wf.retrieveUserCollections().getData());
+			} catch(final IOException e) {
+				return Pair.create(params[0], new Collection[] {});
+			}
+		}
+
+		@Override
+		protected void onPostExecute(final Pair<Spinner, Collection[]> dataPair) {
+			final Collection[] dataSet = dataPair.second;
+
+			if(dataSet.length == 0) {
+				Toast.makeText(MainActivity.this,
+				               "Network issue; no blogs/collections returned.",
+				               Toast.LENGTH_SHORT).show();
+			}
+
+			final String[]  inits = MainActivity.super
+			                                    .getResources()
+			                                    .getStringArray(R.array.blogs_array);
+			final String[]  blogs = Arrays.stream(dataSet)
+			                              .map((final Collection col) -> col.getTitle())
+			                              .toArray((final int size) -> new String[size]);
+			final String[] finals = Arrays.copyOf(inits,
+			                                      inits.length + blogs.length);
+
+			System.arraycopy(blogs, 0, finals, inits.length, blogs.length);
+
+			final ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this.getApplicationContext(),
+			                                                              android.R.layout.simple_spinner_item,
+			                                                              finals);
+
+			adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+			dataPair.first.setAdapter(adapter);
+		}
 	}
 }
